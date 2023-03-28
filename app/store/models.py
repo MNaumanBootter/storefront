4779a91e9c0acdf7ddcf5c98f441c94734cd8a6e
Nauman Bootter <mnaumanbootter@gmail.com>
2023-03-28 10:59:30 +0500

    add Cart and CartItem models

--- app/store/models.py
+++ app/store/models.py
@@ -58,3 +58,12 @@ class OrderItem(models.Model):
     qantity = models.PositiveSmallIntegerField()
     unit_price = models.DecimalField(max_digits=6, decimal_places=2)
 
+
+class Cart(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    qantity = models.PositiveSmallIntegerField()
