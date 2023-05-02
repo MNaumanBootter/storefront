22724a5d86c1e632a248d5ddb33972906512a3f5
Nauman Bootter <mnaumanbootter@gmail.com>
2023-05-02 14:34:42 +0500

    make product description to nullable and blankable

--- app/store/models.py
+++ app/store/models.py
@@ -19,8 +19,7 @@ class Collection(models.Model):
 class Product(models.Model):
     title = models.CharField(max_length=255)
     slug = models.SlugField()
-    description = models.TextField()
-    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+    description = models.TextField(null=True, blank=True)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
