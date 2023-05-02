7f383a7c2c5548b9e381486ac26931ff9b174b87
Nauman Bootter <mnaumanbootter@gmail.com>
2023-05-02 14:36:17 +0500

    add min validation on unit_price in Product model

--- app/store/models.py
+++ app/store/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
 class Promotion(models.Model):
@@ -20,6 +21,10 @@ class Product(models.Model):
     title = models.CharField(max_length=255)
     slug = models.SlugField()
     description = models.TextField(null=True, blank=True)
+    unit_price = models.DecimalField(
+        max_digits=6,
+        decimal_places=2,
+        validators=[MinValueValidator(0.01)])
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
