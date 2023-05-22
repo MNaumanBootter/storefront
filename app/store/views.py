711d809f383253451aa25370464daebc5b552e28
Nauman Bootter <mnaumanbootter@gmail.com>
2023-05-22 19:45:46 +0500

    add searching and ordering in product viewset

--- app/store/views.py
+++ app/store/views.py
@@ -1,6 +1,7 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
+from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework import status
 from store.models import OrderItem
 from store.models import Product, Collection, Review
@@ -11,8 +12,10 @@ from store.serializers import ProductSerializer, CollectionSerializer, ReviewSer
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_class = ProductFilter
+    search_fields = ["title", "description"]
+    ordering_fields = ["unit_price", "last_update"]
 
     def get_serializer_context(self):
         return {'request': self.request}
