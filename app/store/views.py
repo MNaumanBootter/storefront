a32df28e0182eaa94c4fbf870e58c758f38cb083
Nauman Bootter <mnaumanbootter@gmail.com>
2023-05-26 19:32:22 +0500

    add cart delete endpoint

--- app/store/views.py
+++ app/store/views.py
@@ -1,6 +1,10 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.viewsets import ModelViewSet, GenericViewSet
-from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin
+from rest_framework.mixins import (
+    CreateModelMixin,
+    RetrieveModelMixin,
+    DestroyModelMixin,
+)
 from rest_framework.response import Response
 from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework import status
@@ -66,6 +70,7 @@ class ReviewViewSet(ModelViewSet):
 class CartViewSet(
     CreateModelMixin,
     RetrieveModelMixin,
+    DestroyModelMixin,
     GenericViewSet,
 ):
     queryset = Cart.objects.prefetch_related("items__product").all()
