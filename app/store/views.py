a41af3cc94787c8b7cef4137bc0e7ffab0b51648
Nauman Bootter <mnaumanbootter@gmail.com>
2023-05-29 14:47:18 +0500

    add cartitem update / patch endpoint

--- app/store/views.py
+++ app/store/views.py
@@ -18,6 +18,7 @@ from store.serializers import (
     CartSerializer,
     CartItemSerializer,
     AddCartItemSerializer,
+    UpdateCartItemSerializer,
 )
 
 
@@ -80,9 +81,13 @@ class CartViewSet(
 
 
 class CartItemViewSet(ModelViewSet):
+    http_method_names = ["get", "post", "patch", "delete"]
+
     def get_serializer_class(self):
         if self.request.method == "POST":
             return AddCartItemSerializer
+        elif self.request.method == "PATCH":
+            return UpdateCartItemSerializer
         return CartItemSerializer
 
     def get_queryset(self):
