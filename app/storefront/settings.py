9acb54b937b03c7fa7d1a3e6617ca4bc74b8e40f
Nauman Bootter <mnaumanbootter@gmail.com>
2023-06-05 14:32:38 +0500

    add djoser auth endpoints

--- app/storefront/settings.py
+++ app/storefront/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_filters',
     'rest_framework',
+    'djoser',
     'debug_toolbar',
     'store',
     'tags',
@@ -142,9 +143,15 @@ DEBUG_TOOLBAR_CONFIG = {
 
 REST_FRAMEWORK = {
     'COERCE_DECIMAL_TO_STRING': False,
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
     # 'PAGE_SIZE': 10,
     # "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
 }
 
+SIMPLE_JWT = {
+    'AUTH_HEADER_TYPES': ('JWT',),
+}
 
 AUTH_USER_MODEL = "core.User"
