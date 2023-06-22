17019ef61c2afafc3318e3dd63e31e7f34dbca05
Nauman Bootter <mnaumanbootter@gmail.com>
2023-06-22 14:11:57 +0500

    add me route for customers

--- app/store/views.py
+++ app/store/views.py
@@ -9,6 +9,7 @@ from rest_framework.mixins import (
 from rest_framework.response import Response
 from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework import status
+from rest_framework.decorators import action
 from store.pagination import DefaultPagination
 from store.models import (
     Product,
@@ -117,3 +118,15 @@ class CustomerViewSet(
 ):
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
+
+    @action(detail=False, methods=["GET", "PUT"])
+    def me(self, request):
+        (customer, created) = Customer.objects.get_or_create(user_id=request.user.id)
+        if request.method == "GET":
+            serializer = CustomerSerializer(customer)
+
+        elif request.method == "PUT":
+            serializer = CustomerSerializer(customer, data=request.data)
+            serializer.is_valid()
+            serializer.save()
+        return Response(serializer.data)
