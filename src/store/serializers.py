9dff1abdbf02acbf33899a8146cb31546b085830
Nauman Bootter <mnaumanbootter@gmail.com>
2023-07-11 14:21:56 +0500

    add new signal for order_created

--- src/store/serializers.py
+++ src/store/serializers.py
@@ -1,6 +1,7 @@
 from decimal import Decimal
 from django.db import transaction
 from rest_framework import serializers
+from store.signals import order_created
 from store.models import (
     Order,
     OrderItem,
@@ -199,5 +200,7 @@ class CreateOrderSerializer(serializers.Serializer):
             OrderItem.objects.bulk_create(order_items)
 
             Cart.objects.filter(pk=cart_id).delete()
+
+            order_created.send_robust(sender=self.__class__, order=order)
 
             return order
