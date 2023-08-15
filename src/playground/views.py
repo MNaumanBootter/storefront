39b9679eda08773448013ce8710492ee7c2a9809
Nauman Bootter <mnaumanbootter@gmail.com>
2023-08-15 15:21:54 +0500

    add view caching

--- src/playground/views.py
+++ src/playground/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.core.cache import cache
+from django.views.decorators.cache import cache_page
 from playground.tasks import email_customers
 import requests
 
@@ -9,10 +10,12 @@ def email_test(request):
     return HttpResponse("hello")
 
 
+@cache_page(5 * 60)
 def cache_test(request):
-    key = "httpbin_result"
-    if cache.get(key) is None:
-        response = requests.get("https://httpbin.org/delay/2")
-        data = response.json()
-        cache.set(key, data)
-    return HttpResponse(f"{cache.get(key)}")
+    # key = "httpbin_result"
+    # if cache.get(key) is None:
+    response = requests.get("https://httpbin.org/delay/2")
+    data = response.json()
+    # cache.set(key, data)
+    # return HttpResponse(f"{cache.get(key)}")
+    return HttpResponse(data)
